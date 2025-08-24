38616f8105880206fdee6d508a7039aa1e5de8e8
Juergen Pfundt <juergen.pfundt@gmail.com>
2025-08-24 21:36:16 +0200

    update of dependencies added cython and pyinstall build

--- install.bat
+++ install.bat
@@ -4,6 +4,6 @@ del *.c *.pyd
 python3 setup.py build_ext --inplace
 mkdir dist
 move *.pyd dist
-pyinstaller beck-view-gui.spec --noconfirm
+pyinstaller beck-view-movie-gui.spec --noconfirm
 copy /y dist\beck-view-movie-gui-bundle\beck-view-movie-gui.exe "%CD%"
 echo "Executable `beck-view-movie-gui.exe` ready for usage in directory %CD%"
